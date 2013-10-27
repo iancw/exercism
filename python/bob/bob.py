bfc5bdfaae4fda7cee4666e5313e5931af81f225
Ian Will <icwill@gmail.com>
2013-10-27 09:47:09 -0400

    Fixed unicode support in bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,12 +1,12 @@
 
 class Bob:
 
-	def __init__(self):
-		pass
-
 	def hey(self, message):
-		if message is None or len(trim(message)) == 0:
+		if message is None or len(message.strip()) == 0:
 			return 'Fine. Be that way!'
+                # Convert from a string object to a unicode object
+                # for correct umlaut matching
+		message = message.decode('latin-1')
 		if message.isupper():
 			return 'Woah, chill out!'
 		if message.endswith('?'):
